990cea17490894f68120d6140482a237cf6b9f07
Alex Noble <AWGR-Alex@comcast.net>
2018-07-12 14:28:14 -0700

    add addAuthor form

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -41,8 +41,17 @@ function Book({title, onClick}){
   </div>);
 }
 
-function Continue(){
-  return (<div/>);
+function Continue({show, onContinue}){
+  return (
+    <div className="row continue">
+    { show
+      ? <div className="col-11">
+        <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+      </div>
+      : null}
+    }
+  </div>
+  );
 }
 
 function Footer(){
@@ -55,12 +64,12 @@ function Footer(){
 }
 
 
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
     return (
       <div className="container-fluid">
         <Hero/>
         <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-        <Continue/>
+        <Continue show={highlight === 'correct'} onContinue={onContinue}/>
         <p><Link to="/add">Add an author</Link></p>
         <Footer/>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import './index.css';
-import {BrowserRouter, Route} from 'react-router-dom';
 import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import registerServiceWorker from './registerServiceWorker';
 import {shuffle, sample} from 'underscore';
 
@@ -47,52 +48,60 @@ const authors = [
     }
   ];
 
-function getTurnData(authors){
-  const allBooks = authors.reduce(function (p,c,i){
-    return p.concat(c.books);
-  }, []);
-  const fourRandomBooks = shuffle(allBooks).slice(0,4);
-  const answer = sample(fourRandomBooks);
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
 
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    }
+}
+
+function resetState() {
   return {
-    books: fourRandomBooks,
-    author:
-          authors.find((author) =>
-          author.books.some((title)=>
-          title===answer))
-  }
+    turnData: getTurnData(authors),
+    highlight: ''
+  };
 }
 
-const state = {
-  turnData: getTurnData(authors),
-  highlight: ''
-}
-function onAnswerSelected(answer){
-  const isCorrect=state.turnData.author.books.some((book) => book === answer);
-  state.highlight=isCorrect ? 'correct' : 'wrong';
+let state = resetState();
+
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
   render();
 }
 
-function AddAuthorForm({match}){
-  return <div>
-    <h1>Add Author</h1>
-    <p>{JSON.stringify(match)}</p>
-  </div>
+function App() {
+  return <AuthorQuiz {...state}
+    onAnswerSelected={onAnswerSelected}
+    onContinue={() => {
+      state = resetState();
+      render();
+    }}/>;
 }
 
-function App(){
-  return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>
-}
+const AuthorWrapper = withRouter(({ history }) =>
+  <AddAuthorForm onAddAuthor={(author) => {
+    authors.push(author);
+    history.push('/');
+  }} />
+);
 
-function render(){
+function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <React.Fragment>
-        <Route exact path="/" component={App} />
-        <Route path="/add" component={AddAuthorForm} />
-      </React.Fragment>
-    </BrowserRouter>, document.getElementById('root'));
+  <BrowserRouter>
+    <React.Fragment>
+      <Route exact path="/" component={App} />
+      <Route path="/add" component={AuthorWrapper} />
+    </React.Fragment>
+  </BrowserRouter>, document.getElementById('root'));
 }
-
 render();
 registerServiceWorker();
